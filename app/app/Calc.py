a88c9d4aa0fb9b038b8f1473d6af0cbd4dc45b30
Ayush6459 <ayushranjan6459@gmail.com>
2022-11-03 21:28:57 +0530

    database initial configuration

---
+++ app/app/Calc.py
@@ -0,0 +1,13 @@
+"""
+Calculator Functions
+"""
+
+def add(x,y):
+    ''' add x and y and return result'''
+    return x+y 
+def subtract(x,y):
+    '''
+        subtract x from y 
+    '''
+    return y-x 
+
---
+++ app/app/tests.py
@@ -0,0 +1,18 @@
+"""
+Simple Test
+"""
+
+from django.test import SimpleTestCase 
+
+from app import Calc
+
+class calctests(SimpleTestCase):
+    """Test: the calc modules. """
+    def test_add_numbers(self):
+        res = Calc.add(5,8)
+        self.assertEqual(res,13)
+
+    def test_subtract_number(self):
+        "Test subtracting number"
+        res = Calc.subtract(10,15)
+        self.assertEqual(res,5)
