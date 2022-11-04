c70ce87165a7aea97ff4490d5de8cee2d307f0ae
Ayush6459 <ayushranjan6459@gmail.com>
2022-11-04 16:56:20 +0530

    cofigured database

--- app/app/Calc.py
+++ app/app/Calc.py
@@ -2,12 +2,15 @@
 Calculator Functions
 """
 
-def add(x,y):
-    ''' add x and y and return result'''
-    return x+y 
-def subtract(x,y):
-    '''
-        subtract x from y 
-    '''
-    return y-x 
 
+def add(x, y):
+    ''' add x and y and return result'''
+
+    return x+y
+
+
+def subtract(x, y):
+    '''
+        subtract x from y
+    '''
+    return y-x
--- app/app/tests.py
+++ app/app/tests.py
@@ -2,17 +2,18 @@
 Simple Test
 """
 
-from django.test import SimpleTestCase 
+from django.test import SimpleTestCase
 
 from app import Calc
+
 
 class calctests(SimpleTestCase):
     """Test: the calc modules. """
     def test_add_numbers(self):
-        res = Calc.add(5,8)
-        self.assertEqual(res,13)
+        res = Calc.add(5, 8)
+        self.assertEqual(res, 13)
 
     def test_subtract_number(self):
         "Test subtracting number"
-        res = Calc.subtract(10,15)
-        self.assertEqual(res,5)
+        res = Calc.subtract(10, 15)
+        self.assertEqual(res, 5)
